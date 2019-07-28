ac8fad98a7613154622a2b27db8fef9a4168697d
Paulo Sorrentino <inshideru@gmail.com>
2019-07-28 14:39:45 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+const typeDefs = `
+type Query {
+  info: String!
+  feed: [Link]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
+}
+`
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`
+  }
+}
+
+// 3
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
