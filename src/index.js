545f2a1d19a20b84b58347041092e040eee209ff
Paulo Sorrentino <inshideru@gmail.com>
2019-07-28 15:25:50 -0300

    added Link schema

--- src/index.js
+++ src/index.js
@@ -12,15 +12,27 @@ type Link {
   url: String!
 }
 `
+const links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQl'
+}]
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url
   }
 }
 
-// 3
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
 })
+
 server.start(() => console.log(`Server is running on http://localhost:4000`))
